e90e1d405bb60afcbce6a5f083baf2d5e86c5463
Darryl Hodgins <dhodgins@central1.com>
2016-12-15 10:07:31 -0800

    Create a testbed (fake module) for testing, similar to what's in main.ts (bootstrapModule).  Here we go!

--- exercise/src/app/simple/simple.component.test.js
+++ exercise/src/app/simple/simple.component.test.js
@@ -1,9 +1,31 @@
 import { SimpleComponent } from './simple.component';
+import { TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 describe('c1-simple', function () {
     describe('getMessage()', function () {
         it('should return \'A simple component\'', function () {
             var component = new SimpleComponent();
             expect(component.getMessage()).toEqual('A simple component');
+        });
+    });
+    describe('when rendering the template', function () {
+        beforeAll(function () {
+            TestBed.initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting());
+        });
+        beforeEach(function () {
+            TestBed.configureTestingModule({
+                declarations: [
+                    SimpleComponent
+                ]
+            });
+        });
+        it('should show the correct message in a paragraph', function (done) {
+            var fixture = TestBed.createComponent(SimpleComponent);
+            fixture.detectChanges();
+            var debug = fixture.debugElement.query(By.css('p'));
+            var element = debug.nativeElement;
+            expect(element.textContent).toEqual('A simple component');
         });
     });
 });
--- exercise/src/app/simple/simple.component.test.ts
+++ exercise/src/app/simple/simple.component.test.ts
@@ -1,12 +1,48 @@
 import {SimpleComponent} from './simple.component';
+import {TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
 
 
 describe('c1-simple', () => {
+
 	describe('getMessage()', () => {
 		it('should return \'A simple component\'', () => {
 			let component = new SimpleComponent();
 
 			expect(component.getMessage()).toEqual('A simple component');
+		});
+	});
+
+	describe('when rendering the template', () => {
+
+		beforeAll(() => {
+			TestBed.initTestEnvironment(
+				BrowserDynamicTestingModule,
+				platformBrowserDynamicTesting()
+			);
+		});
+
+		beforeEach(() => {
+			TestBed.configureTestingModule({
+				// we don't need to import BrowserModule or anything because we're not using ng-if or anything
+				declarations: [
+					SimpleComponent
+				]
+			});
+		});
+
+		it('should show the correct message in a paragraph', (done) => {
+			let fixture = TestBed.createComponent(SimpleComponent);
+
+			fixture.detectChanges();
+
+			let debug = fixture.debugElement.query(By.css('p'));
+
+			let element = debug.nativeElement as HTMLElement;
+
+			expect (element.textContent).toEqual('A simple component');
+
 		});
 	});
 });
